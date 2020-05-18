b00853d2d88716a0ad896bb46962217183028abc
Lamont <Lamont@DESKTOP-EHRDAOG.Home>
2020-05-17 19:53:26 -0500

    Release 1.0 Added function to add and view database items

--- Pet Database/src/Database.java
+++ Pet Database/src/Database.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
-public class Database {
+public class Database<T extends Pet> {
+	
+	private ArrayList<T> dbList = new ArrayList<T>(); 
+	
+	
+	public void addItem(T item) {
+		
+		
+		dbList.add(item);
+		
+	}
+
+	public void printDb() {
+		
+		
+		
+		
+		String lineSeparator = "+-----------------------------+";
+		
+		System.out.println(lineSeparator);
+		System.out.printf("| %-3s | %-15s | %-3s %n", "ID", "NAME", "AGE");
+		System.out.println(lineSeparator);
+		
+		for(T e : dbList) {
+			System.out.printf("| %-3s | %-15s | %-3s | %n", dbList.indexOf(e), e.getName(), e.getAge());
+			
+		}
+		System.out.println(lineSeparator);
+	}
+
+	
 
 }
---
+++ Pet Database/src/PetDatabase.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class PetDatabase {
+
+	private static Database<Pet> petDb = new Database<Pet>();
+	private static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		petDb.addItem(new Pet("Boomer", 15));
+		petDb.addItem(new Pet("Milkshake", 4));
+		petDb.addItem(new Pet("Millie", 2));
+		petDb.addItem(new Pet("Otis", 12));
+
+		int userInput = menu();
+
+		while (userInput != 7) {
+			System.out.println("Your choice: " + userInput + "\n");
+			switch (userInput) {
+
+			case 1:
+				petDb.printDb();
+				break;
+
+			case 2:
+				String input = addPetDialogue();
+				input.trim();
+				int count = 0;
+				String done = "done";
+				while (!input.contentEquals(done)) {
+
+					String[] tokens = input.split(" ");
+
+					petDb.addItem(new Pet(tokens[0], Integer.parseInt(tokens[1])));
+
+					input = addPetDialogue();
+					count += 1;
+
+				}
+
+				System.out.println(count + " pets added.");
+				break;
+				
+				
+
+			}
+			userInput = menu();
+		}
+	}
+
+
+	private static String addPetDialogue() {
+		System.out.println("add pet (name, age): ");
+		return in.nextLine();
+
+	}
+
+	public static int menu() {
+
+		System.out.println("What would you like to do?");
+
+		return Integer.parseInt(in.nextLine());
+
+	}
+}
